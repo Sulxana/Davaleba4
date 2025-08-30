76e210c7ef8726becf55352e91137c41ef64fc31
Giorgi <Giorgi@GIO>
2025-08-30 19:31:35 +0400

    Commenting everything

--- second.py
+++ second.py
@@ -1,45 +1,45 @@
-import random
-import string
-
-# ამოცანა 4
-strs = input("შემოიყვანე სიტყვა/რიცხვი: ")
-r_str = strs[::-1]
-if strs == r_str:
-    print(True)
-else:
-    pass
-
-# ამოცანა 5
-strs = input("შემოიყვანე სიტყვა: ")
-while strs.count(" ")!=0:
-    strs = input("შემოიყვანე მხოლოდ ერთი სიტყვა: ")
-
-mylist = list(strs)
-for i in range(5):
-    new = "".join(random.sample(mylist,k=len(mylist)))
-    print(new)
-
-# ამოცანა 6
-st = set()
-while True:
-    strs = input("შემოიყვანე რიცხვი:")
-    newlist = strs.split()
-
-    for i in range(len(newlist)):
-        st.add(int(newlist[i]))
-
-    mylist = list(st)
-    mylist = sorted(mylist)
-
-    r_mylist = mylist.copy()
-    r_mylist.reverse()
-
-    rand_mylist = random.sample(mylist,k=len(mylist))
-    q = input("როგორ გსურთ რომ დავასორტიროთ? (კლებადობით/ზრდადობით/რანდომად): ")
-
-    if q == "ზრდადობით":
-        print(st)
-    elif q == "კლებადობით":
-        print(r_mylist)
-    else:
-        print(rand_mylist)
+# import random
+# import string
+#
+# # ამოცანა 4
+# strs = input("შემოიყვანე სიტყვა/რიცხვი: ")
+# r_str = strs[::-1]
+# if strs == r_str:
+#     print(True)
+# else:
+#     pass
+#
+# # ამოცანა 5
+# strs = input("შემოიყვანე სიტყვა: ")
+# while strs.count(" ")!=0:
+#     strs = input("შემოიყვანე მხოლოდ ერთი სიტყვა: ")
+#
+# mylist = list(strs)
+# for i in range(5):
+#     new = "".join(random.sample(mylist,k=len(mylist)))
+#     print(new)
+#
+# # ამოცანა 6
+# st = set()
+# while True:
+#     strs = input("შემოიყვანე რიცხვი:")
+#     newlist = strs.split()
+#
+#     for i in range(len(newlist)):
+#         st.add(int(newlist[i]))
+#
+#     mylist = list(st)
+#     mylist = sorted(mylist)
+#
+#     r_mylist = mylist.copy()
+#     r_mylist.reverse()
+#
+#     rand_mylist = random.sample(mylist,k=len(mylist))
+#     q = input("როგორ გსურთ რომ დავასორტიროთ? (კლებადობით/ზრდადობით/რანდომად): ")
+#
+#     if q == "ზრდადობით":
+#         print(st)
+#     elif q == "კლებადობით":
+#         print(r_mylist)
+#     else:
+#         print(rand_mylist)
--- third.py
+++ third.py
@@ -1,56 +1,56 @@
-# ამოცანა 7
+# # ამოცანა 7
+# #
+# strs = input("შეიყვანე ტექსტი: ")
+# mylist = []
+# for s in strs:
+#     if s.isalpha() or s.isspace():
+#         mylist.append(s)
+# print("".join(mylist))
 #
-strs = input("შეიყვანე ტექსტი: ")
-mylist = []
-for s in strs:
-    if s.isalpha() or s.isspace():
-        mylist.append(s)
-print("".join(mylist))
-
-# ზუსტად იგივე ლოგიკაა უბრალოდ თავიდან ესე გავაკეთე ლისტებად შემოვიტანე და მერე მივხვდები რო პირდაპირ შემეძლო
-
-strs = list(input("შეიყვანე ტექსტი: "))
-mylist = []
-for i in range(len(strs)):
-    if strs[i].isalpha() or strs[i].isspace():
-        mylist.append(strs[i])
-print("".join(mylist))
-
-# ამოცანა 8
-strs = input("შემოიტანეთ სია: ")
-mylist = strs.split()
-mylist = [int(s) for s in mylist]
-
-for i in range(len(mylist)-1):
-    newlist = []
-    for j in range(len(mylist)-1):
-        newlist.append(mylist[j]+mylist[j+1])
-    mylist = newlist
-    print(newlist)
-
-# ამოცანა 9
-strs = input("შეიყვანე ტექსტი:").lower()
-mylist = strs.split()
-word = []
-mx = 0
-
-for s in mylist:
-    total = mylist.count(s)
-
-    if total > mx:
-        mx = total
-        while word:
-            word.pop()
-        word.append(s)
-
-    elif total == mx and word.count(s)==0:
-        word.append(s)
-print(word)
-
-# ამოცანა 10
-strs = input("შეიყვანე წინადადება:")
-mylist = strs.split()
-mydict = {}
-for s in mylist:
-    mydict[s] = len(s)
-print(mydict)
+# # ზუსტად იგივე ლოგიკაა უბრალოდ თავიდან ესე გავაკეთე ლისტებად შემოვიტანე და მერე მივხვდები რო პირდაპირ შემეძლო
+#
+# strs = list(input("შეიყვანე ტექსტი: "))
+# mylist = []
+# for i in range(len(strs)):
+#     if strs[i].isalpha() or strs[i].isspace():
+#         mylist.append(strs[i])
+# print("".join(mylist))
+#
+# # ამოცანა 8
+# strs = input("შემოიტანეთ სია: ")
+# mylist = strs.split()
+# mylist = [int(s) for s in mylist]
+#
+# for i in range(len(mylist)-1):
+#     newlist = []
+#     for j in range(len(mylist)-1):
+#         newlist.append(mylist[j]+mylist[j+1])
+#     mylist = newlist
+#     print(newlist)
+#
+# # ამოცანა 9
+# strs = input("შეიყვანე ტექსტი:").lower()
+# mylist = strs.split()
+# word = []
+# mx = 0
+#
+# for s in mylist:
+#     total = mylist.count(s)
+#
+#     if total > mx:
+#         mx = total
+#         while word:
+#             word.pop()
+#         word.append(s)
+#
+#     elif total == mx and word.count(s)==0:
+#         word.append(s)
+# print(word)
+#
+# # ამოცანა 10
+# strs = input("შეიყვანე წინადადება:")
+# mylist = strs.split()
+# mydict = {}
+# for s in mylist:
+#     mydict[s] = len(s)
+# print(mydict)
